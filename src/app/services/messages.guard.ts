a614cfcef2641183c2627e264cee52b7d1ad0db3
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-01-29 16:07:51 +0100

    auth redirect ?

--- src/app/services/messages.guard.ts
+++ src/app/services/messages.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+import { User } from './interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesGuard implements CanActivate {
+  constructor(private readonly auth: AuthService) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    
+    return this.auth.connectedUser$.pipe(
+      map((user: User) => user != null)
+    );
   }
   
 }
