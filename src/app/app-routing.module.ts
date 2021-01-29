9e407929d73e31d31086e36e4907c361b670c22d
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-01-29 13:33:43 +0100

    page - login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageLoginComponent } from './pages/page-login/page-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PageLoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
