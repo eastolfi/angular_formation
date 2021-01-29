1135702bb634fd4a32eebfb5e35a291aeddb0006
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-01-29 13:34:16 +0100

    pages - messages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageMessagesComponent } from './pages/page-messages/page-messages.component';
 
 const routes: Routes = [
+  {
+    path: 'messages',
+    component: PageMessagesComponent
+  },
   {
     path: '',
     component: PageLoginComponent
