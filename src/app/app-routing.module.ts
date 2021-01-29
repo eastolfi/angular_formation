0d23a87100e7fce4ab0e265b4cc79a852174907e
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-01-29 13:26:02 +0100

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageMentionComponent } from './pages/page-mention/page-mention.component';
 import { PageMessagesComponent } from './pages/page-messages/page-messages.component';
+import { MessagesGuard } from './services/messages.guard';
 
 const routes: Routes = [
   {
@@ -11,7 +12,8 @@ const routes: Routes = [
   },
   {
     path: 'messages',
-    component: PageMessagesComponent
+    component: PageMessagesComponent,
+    canActivate: [ MessagesGuard ]
   },
   {
     path: '',
