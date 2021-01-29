badfbd9252f5fce12f78df9fa5538f4fccf146b9
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2021-01-29 13:28:33 +0100

    ngb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent],
